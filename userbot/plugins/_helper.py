c87421f4f742bd1a839f2223ab83271faf1ad4f2
Patel Harsh <76835687+Harsh-78@users.noreply.github.com>
2021-06-01 16:54:45 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -15,7 +15,7 @@ async def yardim(event):
     tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
     input_str = event.pattern_match.group(1)
     if tgbotusername is not None or personal_input == "text":
-        results = await event.client.inline_query(tgbotusername, "@personalUserbot")
+        results = await event.client.inline_query(tgbotusername, "@personal_public_bot")
         await results[0].click(
             event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
         )
