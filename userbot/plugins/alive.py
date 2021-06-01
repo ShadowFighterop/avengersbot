48e65b372bb53f5987b56afb52fe0d3da8cdfafe
Patel Harsh <76835687+Harsh-78@users.noreply.github.com>
2021-06-01 13:26:42 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,121 +1,122 @@
-
+import time
 from userbot import *
-
-from userbot.utils import *
-
+from personalbot.utils import *
+from userbot.cmdhelp import CmdHelp
+from telethon import events, version
 from telethon.events import NewMessage
-
 from telethon.tl.custom import Dialog
-
 from telethon.tl.types import Channel, Chat, User
+from telethon import version
+from userbot import ALIVE_NAME, StartTime, Personalversion
+from personalbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
+#-------------------------------------------------------------------------------
 
 
-
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Darkbot User"
-
-
+async def reply_id(event):
+    reply_to_id = None
+    if event.sender_id in Config.SUDO_USERS:
+        reply_to_id = event.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    return reply_to_id
 
 ludosudo = Config.SUDO_USERS
-
 if ludosudo:
-
-
-
     sudou = "True"
-
-
-
 else:
-
     sudou = "False"
 
-    
+DEFAULTUSER = ALIVE_NAME or "Personal User"
+Personal_IMG = Config.ALIVE_PIC
+CUSTOM_ALIVE_TEXT = Config.ALIVE_MSG or "YOUR PERSONAL BOT !"
 
-dark = bot.uid
+USERID = bot.uid
 
-
-
-PM_IMG = "https://telegra.ph/file/bd05af18c9b4fc5b57233.mp4"
-
-
-
-pm_caption = "__**🤘『 ÐΛR̷Ꮶ 』★BØT★ ɨz օռʟɨռɛ🤘**__\n\n"
+mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
 
 
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
 
-pm_caption += (
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
 
-    f"           𖤍M͜͡Y͜͡ B͜͡O͜͡S͜͡S͡𖤍\n**『🔥[{DEFAULTUSER}](tg://user?id={dark})🔥』**\n\n"
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
 
-)
+    time_list.reverse()
+    ping_time += ":".join(time_list)
 
-
-
-
-
-
-
-pm_caption += f"🔥**『 ÐΛR̷Ꮶ 』★BØT★🔥  : 1.0** \n\n"
-
-
-
-pm_caption += f" ** ⚔️𝕋𝔼𝕃𝔼𝕋ℍ𝕆ℕ 𝕍𝔼ℝ𝕊𝕀𝕆ℕ ⚔️  : 6.9.0 ** \n\n"
-
-
-
-
-
-
-
-pm_caption += "**⭐𝔻𝔸𝕋𝔸𝔹𝔸𝕊𝔼 𝕊𝕋𝔸𝕋𝕌𝕊⭐ :- 𝔻𝔸𝕋𝔸𝔹𝔸𝕊𝔼 𝕎𝕆ℝ𝕂𝕀ℕ𝔾 ℕ𝕆ℝ𝕄𝔸𝕃𝕃𝕐 **✅\n\n◆"
-
-
+    return ping_time
 
 
-
-pm_caption +=  "**⭕ℂℍ𝔸ℕℕ𝔼𝕃⭕    : [ᴊᴏɪɴ](https://t.me/Dark_bot_support)**\n\n" 
-
-
-
-
-
-
-
-pm_caption += "**⭐ℂℝ𝔼𝔸𝕋𝕆ℝ ⭐: [ ʜᴀʀsʜ ʜᴇʀᴇ](https://t.me/harsh_78)**\n\n"
-
-
-
-pm_caption +=  "[☠️ᴅᴀʀᴋ ʙᴏᴛ ʀᴇᴘᴏ ☠️ ](https://github.com/Harsh-78/Dark-Userbot)\n "    
-
-
-
+uptime = get_readable_time((time.time() - StartTime))
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="alive$"))
-
 @bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
-
 async def amireallyalive(alive):
+    if alive.fwd_from:
+        return
+    reply_to_id = await reply_id(alive)
 
-    await alive.get_chat()
+    if Personal_IMG:
+        Personal_caption = f"**{CUSTOM_ALIVE_TEXT}**\n\n"
 
-    await alive.delete()
+        Personal_caption += f"      __**╚»★BOT INFO★«╝**__\n"
+        Personal_caption += f"**╭────────☆═━┈┈┈━═☆───────╮**\n"
+        Personal_caption += f"**※┄┄➳ BOT OWNER:** {mention}\n"
+        Personal_caption += f"**※┄┄➳ BOT STATUS : WORKING NORMALLY **\n"
+        Personal_caption += f"**※┄┄➳ YOUR BOT VERSION :**`{Personalversion}`\n"
+        Personal_caption += f"**※┄┄➳ 𝚂𝙴𝙲𝚄𝚁𝙸𝚃𝚈 𝚂𝚃𝙰𝚃𝚄𝚂 : NO BUGS AND ERRORS **\n"
+        Personal_caption += f"**※┄┄➳ TELETHON VERSION :** `{version.__version__}`**\n"
+        Personal_caption += f"**※┄┄➳ UPTIME :** `{uptime}\n`"
+        Personal_caption += f"**※┄┄➳ SUDO STATUS: `{sudou}`**\n"
+        Personal_caption += f"**※┄┄➳ 𝙳𝙴𝚅𝙴𝙻𝙿𝙾𝙴𝚁 𝚂𝚃𝙰𝚃𝚄𝚂 : ACTIVE**\n"
+        Personal_caption +=  f"**※┄┄➳ CREATOR : [🇮🇳• ITS A SECRET •🇮🇳](https://t.me/furious_x_y)**\n"
+        Personal_caption += f"**╰────────☆═━┈┈┈━═☆───────╯**\n"
+        Personal_caption += "[✨REPO✨](https://github.com/FURIOUS-XY/personalbot) 🔹 [📜License📜](https://github.com/FURIOUS-XY/personalbot/blob/master/LICENSE)"
 
-    """ For .alive command, check if the bot is running.  """
+        await alive.client.send_file(
+            alive.chat_id, Personal_IMG, caption=Personal_caption, reply_to=reply_to_id
+        )
+        await alive.delete()
+    else:
+        await edit_or_reply(
+            alive,
+            f"**{CUSTOM_ALIVE_TEXT}**\n\n"
+            f"     __**╚»★BOT INFO★«╝**__\n"
+            f"**╭────────☆═━┈┈┈━═☆───────╮**\n"
+            f"**※┄┄➳ BOT STATUS : WORKING NORMALLY **\n"
+            f"**※┄┄➳ YOUR BOT VERSION :**`{Personalversion}`\n"
+            f"**※┄┄➳ 𝚂𝙴𝙲𝚄𝚁𝙸𝚃𝚈 𝚂𝚃𝙰𝚃𝚄𝚂 : NO BUGS AND ERRORS **\n"
+            f"**※┄┄➳ TELETHON VERSION :** `{version.__version__}`**\n"
+            f"**※┄┄➳ UPTIME :** `{uptime}\n`"
+            f"**※┄┄➳ SUDO STATUS: `{sudou}`**\n"
+            f"**※┄┄➳ 𝙳𝙴𝚅𝙴𝙻𝙿𝙾𝙴𝚁 𝚂𝚃𝙰𝚃𝚄𝚂 : ACTIVE**\n"
+            f"**※┄┄➳ CREATOR : [🇮🇳• ITS A SECRET •🇮🇳](https://t.me/furious_x_y)**\n"
+            f"**※┄┄➳ MASTER:** {mention}\n"
+            f"**╰────────☆═━┈┈┈━═☆───────╯**\n"
+            "[✨REPO✨](https://github.com/FURIOUS-XY/PERSONAL-USERBOT) 🔹 [📜License📜](https://github.com/FURIOUS-XY/PERSONAL-USERBOT/blob/master/LICENSE)",
+        )
 
-    await borg.send_file(alive.chat_id, PM_IMG, caption=pm_caption)
-
-    await alive.delete()
-
-
-
-
-
-    
-
-
-
-    
-    
+CmdHelp("alive").add_command(
+  'alive', None, 'Check weather the bot is alive or not'
+  ).add_info(
+  'mar gaya ya jinda hai?'
+).add()
